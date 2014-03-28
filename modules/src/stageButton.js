a167f635f325e6e58d0083d4197e75382744a907
chrisnic <chris@chrisnicholls.co.nz>
2014-03-28 19:31:14 +1300

    New big red stage button

--- modules/src/stageButton.js
+++ modules/src/stageButton.js
@@ -15,31 +15,18 @@ function init(config) {
 	var mod = new MKON.module('Stage', 'Button', id, req,
 
 	function() {   
-
-		// var t = $('#'+this.id);
-
-		// var d = MKON.CONTENT.getVariable(req[1]);	
-
-		// if (d == 'True') {
-		// 	t.find('.button').removeClass('gray');
-		
-		// } else {
-		// 	t.find('.button').addClass('gray');
-	
-		// }
 	
 	} );
 
 	// content for insertion to gridster
 	var content =   '<li id="' + id + '" data-row="1" data-col="1" data-link="' + url + '" data-sizex="1" data-sizey="1">\
 					<div class="options"><div class="remove"><i class="fa fa-times"></i></div></div>\
-					<div class="content blank"><a class="button lightText large square stage" data-defaultClass="stage">S</a></div></li>';
+					<div class="content blank"><div class="stageButton"><div class="inner"><i class="fa fa-level-up fa-4x"></i></div></div></div></li>';
 
 	content = { html: content, x: 2, y: 2, col: col, row: row };
 
 	MKON.CONTENT.addModule(mod, content);
 
 	MKON.CONTENT.addHook(id, 'space', function() { MKON.COMMS.command(com[0]); });
-	// 
-	// updateAPIString();
+
 }
